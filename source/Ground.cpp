49b5b1e7cd20a43b4e53809ef0ba06e297df6255
Andre Uschmann <mail@andre-uschmann.de>
2018-09-30 10:57:57 +0200

    Fixed Ground movement

--- source/Ground.cpp
+++ source/Ground.cpp
@@ -21,7 +21,7 @@ Ground::Ground(SDL_Renderer* renderer, SDL_Texture* sprites)
 const int Ground::GROUND_Y = 475;
 const int Ground::SPRITE_X = 2;
 const int Ground::SPRITE_Y = 54;
-const int Ground::SPRITE_WIDTH = 1198;
+const int Ground::SPRITE_WIDTH = 1200;
 const int Ground::SPRITE_HEIGHT = 12;
 const SDL_Rect Ground::FRAME_SPRITE = {
     Ground::SPRITE_X,
@@ -33,13 +33,10 @@ const SDL_Rect Ground::FRAME_SPRITE = {
 void Ground::move(int offset)
 {
     mRect1.x -= offset;
-    if(mRect1.x <= Ground::SPRITE_WIDTH * -1) {
-        mRect1.x = Ground::SPRITE_WIDTH;
-    }
-
     mRect2.x -= offset;
-    if(mRect2.x <= Ground::SPRITE_WIDTH * -1) {
-        mRect2.x = Ground::SPRITE_WIDTH;
+    if(mRect1.x <= Ground::SPRITE_WIDTH * -1) {
+        mRect1.x += Ground::SPRITE_WIDTH;
+        mRect2.x += Ground::SPRITE_WIDTH;
     }
 }
 
